530b5b138a3f663245b49f26937e36c461f11d29
unknown <jhon.garcia@correo.usa.edu.co>
2019-11-13 17:37:40 -0500

    Cambios en el Carrito

--- MarketPlace/target/MarketPlace-1.0-SNAPSHOT/js/jsCarrito.js
+++ MarketPlace/target/MarketPlace-1.0-SNAPSHOT/js/jsCarrito.js
@@ -1,68 +1,40 @@
-//$(document).on('ready', funcionPrincipal);
-
-/*
-   $(document).ready(function(){
-     $("p").click(function(){
-       $(this).hide();
-     });
-   });
- */
-
-
-/*$(document).ready(function(){
-   $("#boton1").click(function(){
-       //funconPrincipal();
-       var s = "<tr> <td>hola</td> <td>holax2</td><td>XD</td><td>:v</td> \n\\n\
-\n\
-<tr>";
-
-    $("table").append(s);
-  });
-});*/
 function jaja(){
     $("#car").click(function(){
         console.log("aaa");
-       //funconPrincipal();
-       var s = "<tr> <td>hola</td> <td>holax2</td><td>XD</td><td>:v</td> \n\\n\
-\n\
-<tr>";
-
-    $("#tablita").append(s);
+       funconPrincipal();
   });
 }
 
 function funcionPrincipal(){
- 
- 
-
+    
    $.ajax({
         url: "ServletProducto",
         type:"POST",
         data: {
-          idproducto:idproducto
+          idproducto:idproducto,
+          cantidad:cantidad
         },
-        //etCookie(idproducto);
         success: function( result ) {
           console.log(result);
           var obj=JSON.parse(result);
+          var cantidad=$("#cantidad").val();
+          var s = "<tr> <td>"+obj[i].Id+"</td><td>"+obj[i].Nombre+"</td><td>"+obj[i].Precio+"</td><td>"+cantidad+"</td><tr>";
+          $("#tablita").append(s);
+          
+          
         }
   });
   }
   
 
-function sumar(){
-    
-    //var c = document.getElementById()('cant').value;
-    //c = parseInt(c)+1;
+function sumar(){   
+    var c = document.getElementById()('cant').value;
+    c = parseInt(c)+1;
     document.getElementById("#cant").innerHTML = 2;
     
 }
 
 function restar(){
-    
-    //var c = document.getElementById()('cant').value;
-    //c = parseInt(c)-1;
-    document.getElementById("#cant").innerHTML = 2;
-    
+        document.getElementById("#cant").innerHTML = 2;    
 }
 
